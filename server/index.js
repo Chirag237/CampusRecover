1d83880c2f472f964a68fd7b90cad1a3baa30e0c
Chirag237 <103517587+Chirag237@users.noreply.github.com>
2023-11-30 01:13:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,13 +33,13 @@ dotenv.config();
 //     credentials: true
 // }));
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   // const allowedOrigins = ['http://localhost:3000', 'https://nimble-brigadeiros-5fb66e.netlify.app/', 'https://melodic-khapse-860fdf.netlify.app/'];
   // const origin = req.headers.origin;
   // if (allowedOrigins.includes(origin)) {
   //      res.setHeader('Access-Control-Allow-Origin', origin);
   // }
-  res.header('Access-Control-Allow-Origin, *');
+  // res.header('Access-Control-Allow-Origin, *');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
